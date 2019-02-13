ec4b7767bd37aa5f31df895f99d4da96c4db194a
Mayur Wani <mayurw0819@gmail.com>
2019-02-13 14:31:56 -0500

    solve pagination issue

--- app.js
+++ app.js
@@ -3,7 +3,9 @@ const puppeteer = require('puppeteer');
 (async () => {
   try {
     // setup the browser
-    const browser = await puppeteer.launch({ headless: true });
+    const browser = await puppeteer.launch({
+      headless: true
+    });
     // setup new page
     const page = await browser.newPage();
 
@@ -13,8 +15,36 @@ const puppeteer = require('puppeteer');
     );
 
     // setup the URL
-    await page.goto('https://experts.shopify.com/');
+    await page.goto('https://scrapethissite.com/pages/ajax-javascript/');
+
+    // await till certain part of the page loads
+    await page.waitForSelector('.col-md-12');
+    console.log('Its working');
+
+    // get all the year-links from the page
+    const yearLinks = await page.evaluate(() => document.querySelectorAll('a.year-link'))
+    // const yearLinks = await page.$$('.year-link');
+    console.log(yearLinks.length);
+    // for (const year of yearLinks) {
+    //   const text = await year.evaluate('year-link', a => a.innertext)
+    //   console.log(text);
+    // }
+    //
+    for (year of yearLinks) {
+      // const yearLink = yearLinks[i];
+      const button = await page.evaluate(() => document.querySelectorAll('a.year-link'));
+      button.click();
+
+      // await page.waitForSelector('.table');
+      // const lis = await page.$$('.table ');
+      // console.log(lis);
+
+      // loop over each year
+
+    }
   } catch (err) {
     console.log('The error is:', err);
   }
+
+  // await browser.close();
 })();
